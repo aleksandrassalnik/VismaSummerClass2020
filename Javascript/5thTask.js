ac66a801d86c0b735ecca763bbfe4bb6138a3cb8
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-07-31 09:21:18 +0300

    theory questions

--- Javascript/5thTask.js
+++ Javascript/5thTask.js
@@ -1,9 +1,8 @@
 var houses = [{ name: "Targaryen", motto: "Fire and Blood" }, { name: "Stark", motto: "Winter is Coming" }, { name: "Bolton", motto: "Our Blades Are Sharp" }, { name: "Greyjoy", motto: "We Do Not Sow" }, { name: "Tully", motto: "Family, Duty, Honor" }, { name: "Arryn", motto: "As High as Honor" }, { name: "Lannister", motto: "Hear Me Roar!" }, { name: "Tyrell", motto: "Growing Strong" }, { name: "Baratheon", motto: "Ours is the Fury" }, { name: "Martell", motto: "Unbowed, Unbent, Unbroken" }];
 
 function ES5Moto(name) {
-    for (var i = 0; i < houses.length; i++)
-    {
-        if (name === houses[i].name) return houses[i].motto;     
+    for (var i = 0; i < houses.length; i++) {
+        if (name === houses[i].name) return houses[i].motto;
     }
     return 'There\'s no such house';
 }
