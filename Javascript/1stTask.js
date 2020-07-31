42bb9e69ad1edc2c5cd8d7319d1753696785b281
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-07-31 11:21:15 +0300

    Update 1stTask.js
    Fixed first task. Now it prints random colors  from defined ones (yellow, blue, red, green).

--- Javascript/1stTask.js
+++ Javascript/1stTask.js
@@ -2,13 +2,28 @@ const colors = require('colors');
 
 //defining colors that will be used
 const customColor = [colors.red, colors.green, colors.yellow, colors.blue]
+const getColor = [];
 
 for (let i = 0; i < 7; i++) {
     let output = '';
     for (let j = 0; j < i + 1; j++) {
         output += '#';
     }
-    //Gets needed color, so the loop can work with more than 7 rows
-    const getColor = i % 4;
-    console.log(customColor[getColor](output));
+
+    getColor.push(getRandom(i));
+
+    console.log(customColor[getColor[i]](output));
+}
+
+function getRandom(prevColor) {
+    const newColor = Math.floor(4 * Math.random());
+    if (prevColor !== 0)
+    {
+        
+        if (newColor == getColor[prevColor-1]) {
+            return getRandom(prevColor);
+        }
+        else return newColor;
+    }
+    else return newColor;
 }
