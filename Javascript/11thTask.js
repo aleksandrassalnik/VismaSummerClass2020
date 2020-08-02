c341b501002d7b2891f327e80ede7b3016d77ede
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-08-02 21:46:42 +0300

    merge

--- Javascript/11thTask.js
+++ Javascript/11thTask.js
@@ -1,22 +1,5 @@
 const array = [10, 6, [4, 8], 3, [6, 5, [9]]];
 
-<<<<<<< HEAD
 const ArraySum = (arr) => arr.flat(Infinity).reduce((total, value) => total + value);
-=======
-function FlattenArray(arr) {
-    return arr.reduce((previousValue, currentValue) => {
-        return previousValue.concat(Array.isArray(currentValue) ? FlattenArray(currentValue) : currentValue)
-    }, [])
-}
-
-function ArraySum(arr) {
-    const array = FlattenArray(arr);
-    let sum = 0;
-    array.map(number => {
-        sum += number;
-    })
-    return sum;
-}
->>>>>>> goal-3
 
 console.log(ArraySum(array));
