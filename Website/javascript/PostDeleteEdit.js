b7a936d1a39f109d4a1d36bc11cb78be3652374a
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-08-04 11:19:08 +0300

    added Delete method

--- Website/javascript/PostDeleteEdit.js
+++ Website/javascript/PostDeleteEdit.js
@@ -1,3 +1,4 @@
+let editForm = {};
 (() => {
     const parsedUrl = new URL(window.location.href);
     const id = parsedUrl.searchParams.get("id");
@@ -5,44 +6,65 @@
         fetch(`http://localhost:3000/posts/${id}`)
             .then(response => response.json())
             .then(data => {
+                editForm = data;
                 document.getElementById('author').value = data.author;
                 document.getElementById('date').value = data.date;
                 document.getElementById('title').value = data.title;
                 document.getElementById('content').value = data.content;
-                document.getElementById('question').onsubmit = Edit;
-                // console.log(data)
+                const form = document.getElementById('question')
+                form.onsubmit = Edit;
+                const deleteButton = document.createElement('button');
+                deleteButton.onclick = DeletePost;
+                deleteButton.textContent = 'Delete';
+                deleteButton.id = 'delete';
+                deleteButton.type = 'button';
+                form.append(deleteButton);
             })
     }
 })()
+
+async function DeletePost() {
+    const parsedUrl = new URL(window.location.href);
+    const id = parsedUrl.searchParams.get("id");
+    await fetch(`http://localhost:3000/posts/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    window.location = './index.html';
+}
 
 function Edit(e) {
     e.preventDefault();
     const parsedUrl = new URL(window.location.href);
     const id = parsedUrl.searchParams.get("id");
-    const obj = getForm();
+    const obj = getForm(editForm);
     if (obj)
-    Put(obj, id);
+        Put(obj, id)
 }
 
 function postForm() {
     const obj = getForm();
     if (obj)
-    Post(obj);
+        Post(obj)
 }
 
-function getForm() {
+function getForm(obj = null) {
     const form = document.querySelector('#question').elements;
-    let obj = {
-        "author": "",
-        "date": "",
-        "title": "",
-        "content": "",
-        "tags": [],
-        "viewCount": "0",
-        "answerCount": "0",
-        "votesCount": "0",
+    if (!obj) {
+        obj = {
+            "author": "",
+            "date": "",
+            "title": "",
+            "content": "",
+            "tags": [],
+            "viewCount": "0",
+            "answerCount": "0",
+            "votesCount": "0",
+        }
     }
-    for (let i = 0; i < form.length - 1; i++) {
+    for (let i = 0; i < 4; i++) {
         if (form[i].value) {
             obj[form[i].name] = form[i].value;
         } else {
@@ -63,6 +85,7 @@ async function Post(obj) {
         },
         body: JSON.stringify(obj)
     })
+    window.location = './index.html';
 }
 
 async function Put(obj, id) {
@@ -73,4 +96,5 @@ async function Put(obj, id) {
         },
         body: JSON.stringify(obj)
     })
+    window.location = './index.html';
 }
