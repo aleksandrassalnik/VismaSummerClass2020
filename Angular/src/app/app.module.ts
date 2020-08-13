105fbd33525a6f31b018529d31b9aef9037a9072
Aleksandras Salnik <39255282+aleksxx9@users.noreply.github.com>
2020-08-13 14:14:53 +0300

    created left and right side menu components
    Created and styled right and left side menu components, created main question component (without css).  Components still need positioning

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LeftSideMenuComponent } from './left-side-menu/left-side-menu.component';
+import { RightSideMenuComponent } from './right-side-menu/right-side-menu.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LeftSideMenuComponent
+    LeftSideMenuComponent,
+    RightSideMenuComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
